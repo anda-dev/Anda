73e37d41552c0b62e2ed2a7b1fddcb895f43b17a
anda-dev <anda.nacu@gmail.com>
2020-03-26 13:54:12 +0000

    tried db connection in repl.it

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+var session = require('express-session');
+var pg = require('pg');
 const bodyParser = require('body-parser');
+var path = require('path');
 
 const app = express();
 
@@ -8,8 +11,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile('public/index.html');
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
 });
+
+app.post('/auth', function(request, response) {
+	var username = request.body.name;
+	var password = request.body.password;
+  console.log(username);
+  console.log(password);
+
+	/* if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	} */
+});
+
+/* app.get('/', (req, res) => {
+  res.sendFile('public/index.html');
+}); */
 
 app.listen(3000, () => console.log('server started'));
